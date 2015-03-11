1328b9860d4c44268adb0f8a80b533e64c4c1478
Kelvin Cheng <kelvin.cheng@gen-net.com.sg>
2015-03-11 22:02:46 +0800

    resolve the error checking issue where infinte loop will happen when number !=7

--- script/trello.js
+++ script/trello.js
@@ -1,38 +1,41 @@
-
 $("#button").click(function() {
-    var encryptKey=$("#encryptionKey").val();
-    var encryptValue=$("#encryptedValue").val();
-    decrypt(encryptKey,encryptValue);
+    var encryptKey = $("#encryptionKey").val();
+    var encryptValue = $("#encryptedValue").val();
+    decrypt(encryptKey, encryptValue);
 
 });
 
-function decrypt(encryptKey,encryptValue)
-{
-	//h-x/37=h
-	var keyPosition=encryptKey.split("");
-	var decryptValue="";
-	var answer={};
-	while(encryptValue!=7)
-	{
-		answer=divisibleValue(keyPosition.length,encryptValue)
-		decryptValue+=keyPosition[answer.position];
-		encryptValue=answer.value;
-	};
-	decryptValue=decryptValue.split("").reverse().join("");
-	console.log(decryptValue);
+function decrypt(encryptKey, encryptValue) {
+    //h-x/37=h
+    var keyPosition = encryptKey.split("");
+    var decryptValue = "";
+    var answer = {};
+    var stop = false;
+    while (stop == false) {
+        answer = divisibleValue(keyPosition.length, encryptValue)
+        decryptValue += keyPosition[answer.position];
+        encryptValue = answer.value;
+        if (encryptValue == 7) {
+            stop = true;
+        } else if (encryptValue < 7) {
+            stop = true;
+            decryptValue = "Error";
+        }
+    };
+    if (decryptValue != "Error") {
+        decryptValue = decryptValue.split("").reverse().join("");
+    }
+    console.log(decryptValue);
 }
 
-function divisibleValue(keyLength,value)
-{
-	var result={};
-	for(var i=0;i<keyLength;i++)
-	{
-		var calculatedResult=(value-i)/37;
-		if(calculatedResult%1===0)
-		{
-			result.value=calculatedResult;
-			result.position=i;
-		}
-	}
-	return result;
+function divisibleValue(keyLength, value) {
+    var result = {};
+    for (var i = 0; i < keyLength; i++) {
+        var calculatedResult = (value - i) / 37;
+        if (calculatedResult % 1 === 0) {
+            result.value = calculatedResult;
+            result.position = i;
+        }
+    }
+    return result;
 }
